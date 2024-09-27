1bb0d91e8ab525be870439d0196ebcd718f6546f
Gaurab Paudyal <gaurab.191324@ncit.edu.np>
2024-09-27 23:01:47 +0545

    home screen making process

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,30 @@
+import HeaderBox from '@/components/headerBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn={firstname:'Gaurab'};
   return (
-    <div>
-      Home
-    </div>
+  <section className='home'>
+<div className='home-content'>
+  <header className='home-header'>
+<HeaderBox type='greeting'
+title="Welcome"
+user={loggedIn?.firstname || 'Guest'} 
+subtext="Acccesss and mangage your account and transaction efficiently "
+/>
+
+<TotalBalanceBox 
+accounts={[]}
+totalBanks={1}
+totalCurrentBalance={1250.35}
+
+
+/>
+  </header>
+
+</div>
+  </section>
   )
 }
 

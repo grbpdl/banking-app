9407805216caf0662d2f2fad1bad4134f782e361
Gaurab Paudyal <gaurab.191324@ncit.edu.np>
2024-09-29 22:24:13 +0545

    sidebar added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,26 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
+    const loggedIn={$id: "stroms",
+      email: "stroms",
+      userId: "stroms",
+      dwollaCustomerUrl: "stroms",
+      dwollaCustomerId: "stroms",
+      firstName: "stroms",
+      lastName: "stroms",
+      address1: "stroms",
+      city: "stroms",
+      state: "stroms",
+      postalCode: "stroms",
+      dateOfBirth: "stroms",
+      ssn: "stroms",};
     return (
-      <main>
-        sidebar
+      <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
         {children}
       </main>
     );

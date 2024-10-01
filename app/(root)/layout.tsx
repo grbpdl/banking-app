f61fc2487bb1430e17d7dd66f6ef0fd6e851114c
Gaurab Paudyal <gaurab.191324@ncit.edu.np>
2024-10-01 23:31:15 +0545

    card section added in right sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,28 +1,41 @@
+import MobileNav from "@/components/MobileNavbar";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    const loggedIn={$id: "stroms",
-      email: "stroms",
-      userId: "stroms",
-      dwollaCustomerUrl: "stroms",
-      dwollaCustomerId: "stroms",
-      firstName: "stroms",
-      lastName: "stroms",
-      address1: "stroms",
-      city: "stroms",
-      state: "stroms",
-      postalCode: "stroms",
-      dateOfBirth: "stroms",
-      ssn: "stroms",};
-    return (
-      <main className="flex h-screen w-full font-inter">
-        <Sidebar user={loggedIn}/>
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const loggedIn = {
+    $id: "stroms",
+    email: "stroms",
+    userId: "stroms",
+    dwollaCustomerUrl: "stroms",
+    dwollaCustomerId: "stroms",
+    firstName: "stroms",
+    lastName: "stroms",
+    address1: "stroms",
+    city: "stroms",
+    state: "stroms",
+    postalCode: "stroms",
+    dateOfBirth: "stroms",
+    ssn: "stroms",
+  };
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg"
+            width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+
         {children}
-      </main>
-    );
-  }
-  
+      </div>
+    </main>
+  );
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,48 @@
 import HeaderBox from '@/components/headerBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn={firstname:'Gaurab'};
+  const loggedIn = {
+    $id: "stroms",
+    email: "stroms",
+    userId: "stroms",
+    dwollaCustomerUrl: "stroms",
+    dwollaCustomerId: "stroms",
+    firstName: "stroms",
+    lastName: "stroms",
+    address1: "stroms",
+    city: "stroms",
+    state: "stroms",
+    postalCode: "stroms",
+    dateOfBirth: "stroms",
+    ssn: "stroms",
+  };
   return (
-  <section className='home'>
-<div className='home-content'>
-  <header className='home-header'>
-<HeaderBox type='greeting'
-title="Welcome"
-user={loggedIn?.firstname || 'Guest'} 
-subtext="Acccesss and mangage your account and transaction efficiently "
-/>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox type='greeting'
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Acccesss and mangage your account and transaction efficiently "
+          />
 
-<TotalBalanceBox 
-accounts={[]}
-totalBanks={1}
-totalCurrentBalance={1250.35}
-
-
-/>
-  </header>
-
-</div>
-  </section>
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+rescent transaction
+      </div>
+      <RightSidebar
+      user={loggedIn}
+      transactions={[]}
+      banks={[{},{}]}
+      />
+    </section>
   )
 }
 

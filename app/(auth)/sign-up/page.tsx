632242630f0209e1658367201b7b318e0104977f
Gaurab Paudyal <gaurab.191324@ncit.edu.np>
2024-10-02 21:49:23 +0545

    implennt react form and zod from shadci form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const SignUP = () => {
+const SignUp = () => {
   return (
-    <div>
-      Signup
-    </div>
+    <section className='flex-center size-full max-sm:px-6'>
+<AuthForm type='sign-up'/>  
+  </section>
   )
 }
 
-export default SignUP
+export default SignUp

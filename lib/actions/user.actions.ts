63cab579e7e7fe7310e53781d10b8b4a70062b3b
Gaurab Paudyal <gaurab.191324@ncit.edu.np>
2024-10-05 22:24:04 +0545

    appwrite configutation made

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,23 +1,58 @@
 "use server";
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
 
 
 
-export const signIn=async () => {
-    
+
+export const signIn = async () => {
+
     try {
         // mutation/database/make fetch
     } catch (error) {
         console.log(error)
-        
+
     }
 }
-export const signUp=async (userData:SignUpParams) => {
-    
+export const signUp = async (userData: SignUpParams) => {
+    const { email, password, firstName, lastName } = userData;
     try {
-        // mutation/database/make fetch
+
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(
+            ID.unique(),
+            email,
+            password,
+            `${firstName} ${lastName}`
+        );
+
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+        return parseStringify{newUserAccount};
+
     } catch (error) {
         console.log(error)
-        
+
+    }
+}
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+    try {
+        const { account } = await createSessionClient();
+        return await account.get();
+    } catch (error) {
+        return null;
     }
 }
